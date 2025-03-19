e81e88e8538f5651c3abd61f2afc5eff1f011616
fimbulwa <160007822+fimbulwa@users.noreply.github.com>
2025-03-19 14:05:55 +0200

    question3 teamService.kt modification

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
@@ -1,9 +1,25 @@
 package jetbrains.kotlin.course.alias.team
 
 import org.springframework.stereotype.Service
+import jetbrains.kotlin.course.alias.util.Identifier
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
 
 @Service
 class TeamService {
 
-    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> = TODO("Not implemented yet")
+    private val identifierFactory: IdentifierFactory = IdentifierFactory()
+
+    companion object {
+        val teamsStorage: MutableMap<Identifier, Team> = mutableMapOf()
+    }
+
+    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> {
+        val teams = (1..teamsNumber).map {
+            val id = identifierFactory.uniqueIdentifier()
+            val team = Team(id)
+            teamsStorage[id] = team
+            team
+        }
+        return teams
+    }
 }
