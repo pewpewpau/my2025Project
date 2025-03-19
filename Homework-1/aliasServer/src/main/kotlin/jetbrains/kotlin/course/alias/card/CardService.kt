cf6c463de873fc84fcadad089883984f73452fad
fimbulwa <160007822+fimbulwa@users.noreply.github.com>
2025-03-19 15:17:30 +0200

    question5 modification to cardService.kt

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
@@ -1,12 +1,37 @@
 package jetbrains.kotlin.course.alias.card
 
 import org.springframework.stereotype.Service
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
 
 @Service
 class CardService {
-    private fun generateCards(): List<Card> = TODO("Not implemented yet")
 
-    private fun List<String>.toWords(): List<Word> = TODO("Not implemented yet")
+    // Property to generate unique identifiers for each card
+    private val identifierFactory: IdentifierFactory = IdentifierFactory()
 
-    fun getCardByIndex(index: Int): Card = TODO("Not implemented yet")
+    // Property to store the list of cards, initialized by calling generateCards
+    val cards: List<Card> = generateCards()
+
+    companion object {
+
+        const val WORDS_IN_CARD = 4
+        val words: List<String> = listOf("Apple", "Banana", "Carrot", "Dragon", "Egg", "Fox", "Grape", "Horse")
+        val cardsAmount: Int = words.size / WORDS_IN_CARD
+    }
+
+    private fun generateCards(): List<Card> {
+        return words.shuffled()
+            .chunked(WORDS_IN_CARD)
+            .take(cardsAmount)
+            .mapIndexed { index, chunk ->
+                Card(identifierFactory.uniqueIdentifier(), chunk.toWords())
+            }
+    }
+
+    private fun List<String>.toWords(): List<Word> = this.map { Word(it) }
+
+    // Method to retrieve a card by its index
+    fun getCardByIndex(index: Int): Card  {
+        return cards.getOrNull(index) ?: throw IllegalArgumentException("Card not found at index $index")
+    }
 }
