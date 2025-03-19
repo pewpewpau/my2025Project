42f604afcff453d4cfddcfd9ae214e1700da27c8
fimbulwa <160007822+fimbulwa@users.noreply.github.com>
2025-03-19 15:26:58 +0200

    question6 implementing gameResultsService

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsService.kt
@@ -1,10 +1,22 @@
 package jetbrains.kotlin.course.alias.results
 
 import org.springframework.stereotype.Service
+import jetbrains.kotlin.course.alias.team.Team
+import jetbrains.kotlin.course.alias.team.TeamService
+
+typealias GameResult = List<Team>
 
 @Service
 class GameResultsService {
-    fun saveGameResults(result: GameResult): Unit = TODO("Not implemented yet")
+    companion object {
+        val gameHistory: MutableList<GameResult> = mutableListOf()
+    }
 
-    fun getAllGameResults(): List<GameResult> = TODO("Not implemented yet")
+    fun saveGameResults(result: GameResult) {
+        require(result.isNotEmpty()) { "Result cannot be empty" }
+        require(result.all { TeamService.teamsStorage.containsKey(it.id) }) { "Invalid team ID in result" }
+        gameHistory.add(result)
+    }
+
+    fun getAllGameResults(): List<GameResult> = = gameHistory.reversed()
 }
