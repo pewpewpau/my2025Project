46cfa6e544a11505e0535296d23c924bfcc193bc
fimbulwa <160007822+fimbulwa@users.noreply.github.com>
2025-03-19 14:21:52 +0200

    comments to teamService.kt

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
@@ -7,12 +7,15 @@ import jetbrains.kotlin.course.alias.util.IdentifierFactory
 @Service
 class TeamService {
 
+    // Property to generate unique identifiers for teams
     private val identifierFactory: IdentifierFactory = IdentifierFactory()
 
+    // Companion object to hold a storage of all teams
     companion object {
         val teamsStorage: MutableMap<Identifier, Team> = mutableMapOf()
     }
 
+    // Method to generate teams for one round and store them in the storage
     fun generateTeamsForOneRound(teamsNumber: Int): List<Team> {
         val teams = (1..teamsNumber).map {
             val id = identifierFactory.uniqueIdentifier()
