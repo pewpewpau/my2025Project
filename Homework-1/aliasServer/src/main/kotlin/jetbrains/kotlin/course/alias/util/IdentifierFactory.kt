28cfadb3866babc913ea345780d403c140a3d53e
fimbulwa <160007822+fimbulwa@users.noreply.github.com>
2025-03-20 17:45:09 +0200

    identifierFactory class question1

---
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/util/IdentifierFactory.kt
@@ -0,0 +1,11 @@
+package jetbrains.kotlin.course.alias.util
+
+typealias Identifier = Int
+
+class IdentifierFactory {
+    private var counter: Identifier = 0
+
+    fun uniqueIdentifier(): Identifier {
+        return ++counter
+    }
+}
