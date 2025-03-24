5e6c687990acba2d86922a0ca7aca0ee493c8b34
fimbulwa <160007822+fimbulwa@users.noreply.github.com>
2025-03-24 18:42:17 +0200

    ...

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/util/Utils.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/util/Utils.kt
@@ -1,11 +1,1 @@
 package jetbrains.kotlin.course.alias.util
-
-typealias Identifier = Int
-
-class IdentifierFactory {
-    private var counter: Identifier = 0
-
-    fun uniqueIdentifier(): Identifier {
-        return ++counter
-    }
-}
